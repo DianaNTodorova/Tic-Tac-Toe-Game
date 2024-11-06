c9f2babbf103d77e48b7969f7486cf714a150491
Diana Todorova <Diana.Todorova@stud.sundsgarden.se>
2024-11-06 11:57:39 +0100

    main

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: 'https://dianantodorova.github.io/Tic-Tac-Toe-Game/', // Replace with your repository name
+});
